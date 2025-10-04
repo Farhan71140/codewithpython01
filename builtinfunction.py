ded58c651bfc53f784f4c33e10f84262d576c709
Farhan <mohdfarhanuddin002@gmail.com>
2025-10-04 06:05:12 +0530

    Added python files from class05

---
+++ builtinfunction.py
@@ -0,0 +1,16 @@
+# Using built-in functions
+name = "Python"
+numbers = [10, 20, 30, 40]
+
+print("Name:", name)                     
+print("Length of name:", len(name))      
+print("Type of numbers:", type(numbers)) 
+print("Maximum number:", max(numbers))   
+print("Sum of numbers:", sum(numbers))   
+
+
+#- print() – displays output
+#- len() – returns the length of an object
+#- type() – returns the type of a variable
+#- max() / min() – returns the largest/smallest value
+#- sum() – adds up elements in a list
---
+++ functiondefinition.py
@@ -0,0 +1,17 @@
+# Define a function to calculate the area of a rectangle
+def calculate_area(length, width):
+    area = length * width
+    return area
+
+# Define a function to greet a user
+def greet_user(name):
+    print(f"Hello, {name}! Welcome to Python.")
+
+# Define a function to check if a number is positive
+def is_positive(number):
+    return number > 0
+
+# Calling the functions
+print("Area:", calculate_area(5, 3))   
+greet_user("FARHAN")                     
+print(is_positive(-2))                   
