bd50f75dbe6334bbfbc8c93f8aa0db1cb1662c3c
Farhan <mohdfarhanuddin002@gmail.com>
2025-10-07 11:17:51 +0530

    Added problem17.py from problemsolving2

---
+++ problem17.py
@@ -0,0 +1,5 @@
+# Question: Write a program to count the number of zeroes in the following tuple.
+
+numbers = (0, 1, 2, 0, 3, 0, 4, 5, 0)  # Example tuple with some zeroes
+zero_count = numbers.count(0)  # Use the count() method to count zeroes
+print("Number of zeroes in the tuple:", zero_count)  # Display the result
