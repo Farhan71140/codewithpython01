21e5568d7d1e586a5f81770db4e29fd1fe8cade9
Farhan <mohdfarhanuddin002@gmail.com>
2025-10-21 22:05:43 +0530

    added problem36.py from mypythonfiles

---
+++ problem36.py
@@ -0,0 +1,9 @@
+# Question:
+# Write a Python program to print the following star pattern: *, **, ***
+
+# Input: Number of lines
+n = 3
+
+# Loop to print each line
+for i in range(1, n + 1):
+    print("*" * i)
